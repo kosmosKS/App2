aaf33a0acc3c11670c209b2e12c4b898f4c16edc
k240 <k240@SAM.local>
2023-05-31 12:47:56 +0300

    karpova

---
+++ app/src/main/java/ru/rsue/karpova/BookListActivity.java
@@ -0,0 +1,10 @@
+package ru.rsue.karpova;
+
+import androidx.fragment.app.Fragment;
+
+public class BookListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new BookListFragment();
+    }
+}
